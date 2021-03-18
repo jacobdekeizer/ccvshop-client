aebba7596e436ad82388fd6c37f667277c1972aa
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2021-03-18 13:51:07 +0100

    Generated webhook models

--- generator/SchemaGenerator.php
+++ generator/SchemaGenerator.php
@@ -107,6 +107,13 @@ class SchemaGenerator
         );
         $generator->generate('/API/Schema/vnd.verto.webshop.resource.ordernotifications.v1.json', $namespace);
         $generator->generate('/API/Schema/internal.resource.ordernotifications.input.v1.json', $namespace);
+
+        // webhooks
+        $namespace = 'Webhooks';
+        $generator->generate('/API/Schema/vnd.verto.webshop.resource.webhooks.v1.json', $namespace);
+        $generator->generate('/API/Schema/vnd.verto.webshop.resource.collection.webhooks.v1.json', $namespace);
+        $generator->generate('/API/Schema/internal.resource.webhooks.patch.v1.json', $namespace);
+        $generator->generate('/API/Schema/internal.resource.webhooks.post.v1.json', $namespace);
     }
 
     public function generate(string $url, string $namespacePrefix): void
